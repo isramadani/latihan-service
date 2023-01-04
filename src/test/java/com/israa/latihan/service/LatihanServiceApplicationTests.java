e88424af2a11181771d459c484690dddc0f7a3cd
isramadani <113502867+isramadani@users.noreply.github.com>
2023-01-04 14:55:37 +0700

    Add files via upload

---
+++ src/test/java/com/israa/latihan/service/LatihanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israa.latihan.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatihanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
